a04ece48966749a17a3875d43d9102037c8a11be
Erixk90 <erickleonesreyes@gmail.com>
2023-08-21 21:31:51 -0500

    add Navbar

--- cvm-tsx/src/components/Navbar.tsx
+++ cvm-tsx/src/components/Navbar.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+
+import '../App.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbarContent'  >
+    <p>Logo</p>
+    <input type="text" />
+    <nav >
+      <ul className='navContainer'>
+        <li className='navContent'>comics</li>
+        <li className='navContent'>Manga</li>
+        <li className='navContent'>Tienda</li>
+        <li className='navContent'>Login</li>
+        <li className='navContent'>Sign Up</li>
+      </ul>
+    </nav>
+    </div>
   )
 }
 
